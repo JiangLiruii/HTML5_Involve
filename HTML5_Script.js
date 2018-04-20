edb548707d7a1660aa9da85e8cd36e84fda57f71
Big_Panda <535904200@qq.com>
2018-04-20 18:59:00 +0800

    formating pages

--- HTML5_Script.js
+++ HTML5_Script.js
@@ -1,5 +1,4 @@
 document.getElementById('demo').style.fontSize = "25px";
 document.getElementById('demo').style.color = 'red';
 document.getElementById('demo').style.backgroundColor = 'yellow'
-document.getElementById('image').src = 'image.img' <
-  noscript > Sorry your browser not support js < /noscript>
+document.getElementById('image').src = 'image.img'
