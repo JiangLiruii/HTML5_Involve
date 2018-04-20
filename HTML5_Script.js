0230622d6eed23b7ce2efbec5bcd5a58500e8cc8
Big_Panda <535904200@qq.com>
2018-04-20 18:57:27 +0800

    update script

---
+++ HTML5_Script.js
@@ -0,0 +1,5 @@
+document.getElementById('demo').style.fontSize = "25px";
+document.getElementById('demo').style.color = 'red';
+document.getElementById('demo').style.backgroundColor = 'yellow'
+document.getElementById('image').src = 'image.img' <
+  noscript > Sorry your browser not support js < /noscript>
